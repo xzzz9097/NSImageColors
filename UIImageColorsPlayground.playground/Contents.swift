d75c9b69aea7c2d7090f310bbb323eb2a022e057
John Doe <xzzz9097@gmail.com>
2017-01-02 23:05:21 +0100

    Added more samples

--- UIImageColorsPlayground.playground/Contents.swift
+++ UIImageColorsPlayground.playground/Contents.swift
@@ -6,6 +6,16 @@ import UIImageColors
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let image = NSImage(named: "Black on Both Sides.png")
+let images = [NSImage(named: "Black on Both Sides.png"),
+              NSImage(named: "Cupid Deluxe.png"),
+              NSImage(named: "If You're Reading This It's Too Late.png"),
+              NSImage(named: "Kind of Blue.png"),
+              NSImage(named: "My Beautiful Dark Twisted Fantasy.png")]
 
-let color = image?.getColors().primaryColor
+var colors: [NSColor] = []
+
+for image in images {
+    colors.append((image?.getColors().backgroundColor)!)
+}
+
+colors
